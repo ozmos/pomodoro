99bba694176e6526d1791c93b42cc6853c65e393
ozmos <osamumorozumi@gmail.com>
2018-12-02 19:35:20 +0800

    Updates

---
+++ precache-manifest.b156e9d089fecf6c0b1850d903abe8c8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "cb1f1e4f98ed34973a2dbc395fa87990",
+    "url": "/ozmos/pomodoro/static/media/080_simple-sevenths-epiano.cb1f1e4f.mp3"
+  },
+  {
+    "revision": "a544aaea4030188a90f7",
+    "url": "/ozmos/pomodoro/static/js/runtime~main.a544aaea.js"
+  },
+  {
+    "revision": "71ef4967b410f5703463",
+    "url": "/ozmos/pomodoro/static/js/main.71ef4967.chunk.js"
+  },
+  {
+    "revision": "624cdec2563e58508af7",
+    "url": "/ozmos/pomodoro/static/js/1.624cdec2.chunk.js"
+  },
+  {
+    "revision": "71ef4967b410f5703463",
+    "url": "/ozmos/pomodoro/static/css/main.821d12ec.chunk.css"
+  },
+  {
+    "revision": "c681082f0800c762b72b15cdd6d625e6",
+    "url": "/ozmos/pomodoro/index.html"
+  }
+];
